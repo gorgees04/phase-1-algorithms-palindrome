b0a80d7accf9d7711cb55eb2f2189789b8c1c310
Gorgees Odisho <gorgees041@gmail.com>
2023-10-10 20:29:58 +0300

    SOLVE THE CODE WITH ALGARITHM DONE

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const backWord = []
+  for (let i = word.length - 1; i >= 0; i--) {
+    backWord.push(word[i])
+  }
+  
+  if (backWord.join("") === word) {
+    return true
+  } else {
+    return false
+  }
 }
 
+
 /* 
-  Add your pseudocode here
+  Pseudocode
+  - make an variable given backword word using for loop reverse
+  - chech if word and backword word are same 
+    if they are same return true
+    if not the same return false 
 */
 
 /*
-  Add written explanation of your solution here
+  explanation of solution:
+  - simply just make a for loop and push to an array from last letter to first one 
+  - join that backword array to make it as a word 
+  - compare word with the backword I made 
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +38,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("anna"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("sam"));
 }
 
 module.exports = isPalindrome;
